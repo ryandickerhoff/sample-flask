855a15901b6673e1c818f5d277974afda84c0579
ryandickerhoff <62120737+ryandickerhoff@users.noreply.github.com>
2021-01-07 10:32:09 -0500

    initial dash incorporation

--- app.py
+++ app.py
@@ -1,9 +1,45 @@
-from flask import Flask
-from flask import render_template
+# from flask import Flask
+# from flask import render_template
 
-app = Flask(__name__)
+#-
+import os
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+#-
+
+#-
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets) # app = Flask(__name__)
+
+server = app.server
+
+app.layout = html.Div([
+    html.H2('Hello World'),
+    dcc.Dropdown(
+        id='dropdown',
+        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
+        value='LA'
+    ),
+    html.Div(id='display-value')
+])
+
+@app.callback(dash.dependencies.Output('display-value', 'children'),
+              [dash.dependencies.Input('dropdown', 'value')])
+def display_value(value):
+    return 'You have selected "{}"'.format(value)
+#-
 
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
+# @app.route("/")
+# def hello_world():
+#     return "Hello"#render_template("index.html")
+
+#-
+if __name__ == '__main__':
+    app.run_server(
+        debug=True
+    )
+#-
